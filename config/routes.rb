9e1fc1be3b8edb39bced575fa8b2908fde4fef7f
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2020-12-06 02:32:08 +0000

    apply bootstrap template

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  
+
+  root "users/homes#top"
+
+  get 'homes/top'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  
+
   devise_for :admins, controllers: {
   sessions: 'admins/sessions'
   }
-  
+
 end
