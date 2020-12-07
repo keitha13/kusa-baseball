225f5706c8f60debcbb331136c0bf96e0e1eea60
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2020-12-07 11:45:50 +0000

    create posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_010154) do
+ActiveRecord::Schema.define(version: 2020_12_07_113940) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,35 @@ ActiveRecord::Schema.define(version: 2020_12_06_010154) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "AB"
+    t.integer "single"
+    t.integer "double"
+    t.integer "triple"
+    t.integer "HR"
+    t.integer "RBI"
+    t.integer "run"
+    t.integer "BB_HBP"
+    t.integer "steal"
+    t.decimal "inning"
+    t.integer "hit_allowed"
+    t.integer "run_allowed"
+    t.integer "SO"
+    t.integer "BB_HBP_allowed"
+    t.text "body"
+    t.string "post_image_id"
+    t.string "youtube_url"
+    t.string "team_against"
+    t.integer "score_my_team"
+    t.integer "score_team_against"
+    t.datetime "date"
+    t.string "win_lose"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
