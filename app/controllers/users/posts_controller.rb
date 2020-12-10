dae59344a04cab08c5ac896c5358a4ee880f4187
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2020-12-10 12:25:05 +0000

    start setting fontawesome

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,33 +1,53 @@
 class Users::PostsController < ApplicationController
   def new
     @post = Post.new
+    @user = current_user
   end
 
   def index
     @posts = Post.all
+    @user = current_user
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = current_user
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+
+    # YoutubeURLの埋め込み
+    url = params[:post][:youtube_url]
+    url = url.last(11)
+    @post.youtube_url = url
+    # ここまで
+
     @post.save
     redirect_to posts_path
   end
 
   def edit
+    @user = current_user
     @post = Post.find(params[:id])
+    if @post.user == current_user
+      render "edit"
+    else
+      redirect_to posts_path
+    end
   end
 
   def update
     @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
   end
 
   def destroy
     @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
