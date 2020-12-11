ab404612678717d83eddf55efee1b029ffeb1cde
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2020-12-11 05:11:30 +0000

    develop comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+
 
   def after_sign_in_path_for(resource)
     case resource
     when Admin
       admin_path
     when User
-      users_path
+      posts_path
     end
   end
 end
