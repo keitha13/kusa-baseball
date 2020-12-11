41bac3bc78540dddc39643c82eb5b9183a81f0af
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2020-12-11 09:00:47 +0000

    develop follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
-  
+
   enum active_areas: {
     北海道:1,青森県:2,岩手県:3,宮城県:4,秋田県:5,山形県:6,福島県:7,
     茨城県:8,栃木県:9,群馬県:10,埼玉県:11,千葉県:12,東京都:13,神奈川県:14,
@@ -18,7 +19,7 @@ class User < ApplicationRecord
     徳島県:36,香川県:37,愛媛県:38,高知県:39,
     福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47
   }
-  
+
   def active_for_authentication?
     super && (self.is_active == true)
   end
