2eb2e22525aadae1fbf4b10927d7b44ed34ff77f
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2020-12-18 00:38:56 +0000

    upgrade image posting to multiple

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,23 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require popper
+
+  $(function(){
+    $('#post_post_image').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $("#preview").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    });
+  });
+
+ $(function(){
+    $('#user_profile_image').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $("#preview").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    });
+  });
