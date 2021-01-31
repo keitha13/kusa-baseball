66dce29acf194ba3a1b30853631c4c0a2afabb8a
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2021-01-31 03:05:20 +0000

    modify session and registration footer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,18 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'helpers/capybara.rb'
 
+Dir[Rails.root.join("spec/helpers/*.rb")].each { |f| require f }
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
+  config.include Capybara::DSL
+end
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe '投稿のテスト', type: :system do
+  let(:user) { create(:user) }
+  let!(:user2) { create(:user) }
+  let!(:post) { create(:post, user: user) }
+  let!(:post2) { create(:post, user: user2) }
+  before do
+    visit new_user_session_path
+  	fill_in 'user[email]', with: user.email
+  	fill_in 'user[password]', with: user.password
+  	click_button 'ログイン'
+  end
+  describe '新規投稿のテスト' do
+    context '新規投稿画面への遷移' do
+      it '遷移できる' do
+      	visit new_post_path
+      	expect(current_path).to eq('/posts/new')
+      end
+    end
+    context '表示の確認' do
+      before do
+        visit new_post_path
+      end
+      it '新規投稿と表示される' do
+        expect(page).to have_content('新規投稿')
+      end
+    end
+  end
+end
