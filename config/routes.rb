7ce86b707a429c6a6bae385313b5ca7966149dc3
EC2 Default User <ec2-user@ip-172-31-43-240.ec2.internal>
2021-02-01 12:41:57 +0000

    develop user index

--- config/routes.rb
+++ config/routes.rb
@@ -43,5 +43,6 @@ Rails.application.routes.draw do
 
   namespace :admins do
     resources :posts, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit, :update]
   end
 end
